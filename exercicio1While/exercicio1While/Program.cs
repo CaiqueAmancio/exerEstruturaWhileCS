994a3e7b19ac151aa3877f7787f6d1d642d850f9
CaiqueAmancio <caique.amancio@gmail.com>
2021-07-01 00:54:26 -0300

    primeiro commit

---
+++ exercicio1While/exercicio1While/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace exercicio1While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Olá, Digite a senha para continuar: ");
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Inválida");
+                Console.WriteLine();
+                Console.Write("Digite novamente: ");
+                Console.WriteLine();
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Acesso Permitido!");
+        }
+        
+    }
+
+}
